6f35dfd5750156521043cc22e05de9e9b6dcd2f2
govind <joshigovind740@gmail.com>
2023-12-06 16:17:05 +0530

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <header>
+      <a href="" className='logo'>MyBlog</a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  )
+}
+
+export default App
